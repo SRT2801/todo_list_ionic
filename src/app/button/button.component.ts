5b601b2966ee4c213bc3579eb4a19e3f2692bd61
sahir ruiz <sahirruiz4@gmail.com>
2024-08-10 19:58:48 -0500

    my first aplication with ionic

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent  implements OnInit {
+
+  @Input() value = "";
+  @Input() color = "primary";
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+  
+  doClick(){
+    console.log("added successfully!!");
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface ITask {
+  title: string;
+  description: string;
+  done: boolean;
+  id: number;
+
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +15,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public title!: FormControl;
+  public description!: FormControl;
+  public done!: FormControl;
 
-  constructor() {}
+  public taskForm!: FormGroup;
 
+  public tasks: ITask[] = [];
+
+
+  constructor() {
+    this.initForm();
+  }
+
+  public addTask() {
+    console.log(this.taskForm.value);
+    this.tasks.push({ ...this.taskForm.value, done: false }); //
+    console.log(this.tasks);
+    this.taskForm.reset();
+
+  }
+
+  updateTaskStatus(task: { done: any; }, event: { detail: { checked: any; }; }) {
+    task.done = event.detail.checked;
+
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  private initForm() {
+    this.title = new FormControl('', [Validators.required, Validators.minLength(3)]);
+    this.description = new FormControl('', [Validators.required, Validators.minLength(10)]);
+    this.done = new FormControl(false, []);
+
+    this.taskForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+      done: this.done,
+    });
+
+
+  }
 }
