eda368115acd86619de240d33c9178cc5fc7d10c
sahir ruiz <sahirruiz4@gmail.com>
2024-08-12 12:44:47 -0500

    se aplican mejoras

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'todo_list',
+  appName: 'Todo List App',
   webDir: 'www'
 };
 
